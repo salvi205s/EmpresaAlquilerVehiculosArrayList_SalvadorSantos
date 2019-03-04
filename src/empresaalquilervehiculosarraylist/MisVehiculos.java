cf875ba779d35a7f657dca752c28f7e55765cca9
salvi205 <salvi205s@gmail.com>
2019-03-04 18:02:55 +0100

    Posteriormente, se ordenarán los clientes y los vehículos usando los métodos apropiados.

--- src/empresaalquilervehiculosarraylist/MisVehiculos.java
+++ src/empresaalquilervehiculosarraylist/MisVehiculos.java
@@ -28,21 +28,30 @@ public class MisVehiculos {
         easydrive.imprimirVehiculos();
         //-------------------------------------------------------------------------------------------------
 //El programa solicitará por teclado el nif de un cliente, la matrícula de un vehículo y el número de días que se quiere alquilar.
-
-        System.out.println("Introduce un nif");
-        String nif = entrada.nextLine();
-
-        System.out.println("Introduce una matricula");
-        String matricula = entrada.nextLine();
-
-        System.out.println("Introduce los dias que se va a alquilar el vehiculo");
-        int diasAlquiler = entrada.nextInt();
-
-//El sistema registrará, usando el método apropiado, el alquiler anterior.
-        easydrive.alquilarVehiculo(matricula, nif, diasAlquiler);
-
-        System.out.println("---------------------");
-        System.out.println(easydrive.getAlquileres());
+//
+//        System.out.println("Introduce un nif");
+//        String nif = entrada.nextLine();
+//
+//        System.out.println("Introduce una matricula");
+//        String matricula = entrada.nextLine();
+//
+//        System.out.println("Introduce los dias que se va a alquilar el vehiculo");
+//        int diasAlquiler = entrada.nextInt();
+//
+////El sistema registrará, usando el método apropiado, el alquiler anterior.
+//        easydrive.alquilarVehiculo(matricula, nif, diasAlquiler);
+//
+//        System.out.println("---------------------");
+//        System.out.println(easydrive.getAlquileres());
+//------------------------------------------------------------------------------------------------------------------
+//Posteriormente, se ordenarán los clientes y los vehículos usando los métodos apropiados. 
+        System.out.println("Listas ordenadas");
+        easydrive.ordenarCarteraClientes();
+        easydrive.ordenarCatalogoVehiculos();
+        
+        easydrive.imprimirClientes();
+        easydrive.imprimirVehiculos();
+//------------------------------------------------------------------------------------------------------------------
 
     }
 }
