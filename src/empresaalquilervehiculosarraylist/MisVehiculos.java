67cff6f7d7820e07ddf19ff2eed604d6ec4d2a32
salvi205 <salvi205s@gmail.com>
2019-03-04 17:33:47 +0100

    Se llenarán los arraylist de clientes y vehículos de forma aleatoria usando los métodos pertinentes.

--- src/empresaalquilervehiculosarraylist/MisVehiculos.java
+++ src/empresaalquilervehiculosarraylist/MisVehiculos.java
@@ -16,78 +16,10 @@ public class MisVehiculos {
     
     public static void main(String[] args) {
         //Crear una empresa de alquiler con los datos que tú quieras.
-
         EmpresaAlquilerVehiculos easydrive = new EmpresaAlquilerVehiculos("A-28-187189", "easy drive", "www.easydrive.com");
-        Scanner entrada=new Scanner(System.in);
-//        
-//        //Registro los clientes de la empresa
-//        easydrive.registrarCliente(new Cliente("X5618927C", "Juan", "González López"));
-//
-//        easydrive.registrarCliente(new Cliente("Z7568991Y", "Luis", "Fernández Gómez"));
-//
-//        // registro de los vehículos de la empresa
-//        easydrive.registrarVehiculo(new Vehiculo("4060 TUR", "Skoda", "Fabia", "Blanco", 90.0, false));
-//
-//        easydrive.registrarVehiculo(new Vehiculo("4070 DEP", "Ford", "Mustang", "Rojo", 150.0, true));
-//
-//        easydrive.registrarVehiculo(new Vehiculo("4080 TUR", "VW", "GTI", "Azul", 110.0, false));
-//
-//        easydrive.registrarVehiculo(new Vehiculo("4090 TUR", "SEAT", "Ibiza", "Blanco", 90.0, false));
-//
-//        easydrive.registrarVehiculo(new Vehiculo("4100 FUR", "Fiat", "Ducato", "Azul", 80.0, true));
-//        
-//        
-        // imprime la relación de clientes de easydrive
-//        easydrive.imprimirClientes();
-//
-//        System.out.println("----------------------------------------------------------------------");
-//
-//        // imprime el catálogo de vehículos de easydrive
-//        easydrive.imprimirVehiculos();
-//        
-//        System.out.println("----------------------------------------------------------------------");
-////        System.out.println("Aleatorios "+Cliente.clienteAleatorio());
-////        System.out.println("Aleatorios "+Vehiculo.cocheAleatorio());
-//        
-//        easydrive.rellenarArrayCoches();
-//
-//        easydrive.ordenarCatalogoVehiculos();
-//
-//        easydrive.imprimirVehiculos();
-//        
-//        //---------------------------------------------------
-//
-//        easydrive.rellenarArrayClientes();
-//
-//        easydrive.ordenarCarteraClientes();
-//
-//        easydrive.imprimirClientes();
-//
-//        //------------------------------------------------------------
-////        System.out.println("Introduce matricula");
-////        String matricula=entrada.nextLine();
-////        
-////        easydrive.busquedaBinariaMatricula(matricula);
-////
-////            if (easydrive.busquedaBinariaMatricula(matricula)==-1) {
-////                System.out.println("No se ha encontrado");
-////        }else{
-////                System.out.println("La matricula buscada esta en la posicion : "+easydrive.busquedaBinariaMatricula(matricula));
-////            }
-//            
-//        //-------------------------------------------------------------------------
-//        //DNI
-//            
-//        System.out.println("Introduce DNI");
-//        
-//        String nif=entrada.nextLine();
-//        
-//        easydrive.busquedaBinariaNif(nif);
-//
-//            if (easydrive.busquedaBinariaNif(nif)==-1) {
-//                System.out.println("No se ha encontrado");
-//        }else{
-//                System.out.println("El DNI buscado esta en la posicion : "+easydrive.busquedaBinariaNif(nif));
-//            }    
+      
+        //Se llenarán los arraylist de clientes y vehículos de forma aleatoria usando los métodos pertinentes. 
+        easydrive.rellenarArrayClientes();
+        easydrive.rellenarArrayCoches();
     }
 }
